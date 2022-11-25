daaa745889826fcc47004e15583262ee9499b3d6
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-25 22:25:38 +0600

    fll

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/', (req, res) => {
         res.status(200).json({message:'Welcome to my application', app: 'Natours'});
 })
-app.post('/', (req, res) => {res.send('you can post this to endpoints at https://')});
+app.post('/', (req, res) => {res.send('you can post this to endpoints at https:// it is post request')});
 const port = 3000;
 app.listen(port,()=>{
 console.log(`listening on port ${port}!`);

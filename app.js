9286fc4d47c9790555b8aac42213fd64855f6af8
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-25 20:14:40 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+        res.status(200).json({message:'Welcome to my application', app: 'Natours'});
+})
+app.post('/', (req, res) => {res.send('you can post this to endpoints at https://')});
+const port = 3000;
+app.listen(port,()=>{
+console.log(`listening on port ${port}!`);
+})

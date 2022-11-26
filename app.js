894cf143a57a251e6d49ab9f81d94c79e9aac134
Mohammad Parvez Musharaf <mdparvezmusharaf2@gmail.com>
2022-11-26 23:28:29 +0600

    use of params for detact id

--- app.js
+++ app.js
@@ -20,12 +20,41 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  if (!tours || id > tours.length) {
+    return res.status(404).json({ status: 'success', message: 'invalid' });
+  }
+
+  const id = req.params.id * 1; //multiply one for make it number;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-  //const tour = req.body;
- console.log(req.body);
- res.send('done');
-  // tours.push(tour);
-});  
+  const newId = tours[tours.length - 1].id + 1;
+  const newTours = Object.assign({ id: newId }, req.body);
+  tours.push(newTours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTours,
+        },
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
